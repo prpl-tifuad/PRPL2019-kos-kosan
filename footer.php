e0561328daf69efe33a699cf244f16c243234890
RefaldiErgian <refaldi1700018013@webmail.uad.ac.id>
2019-04-14 11:21:38 +0700

    Refaldi Ergian (1700018013)

---
+++ footer.php
@@ -0,0 +1,67 @@
+<!-- START OF FOOTER FUNC CSS -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+
+	<link rel="stylesheet" href="css/spec_footer/demo.css">
+	<link rel="stylesheet" href="css/spec_footer/footer-distributed-with-address-and-phones.css">
+	
+	<link rel="stylesheet" href="css/spec_footer/font-awesome.min.css">
+
+	<link href="css/spec_footer/cookie.css" rel="stylesheet" type="text/css">
+<!-- END OF FOOTER FUNC CSS -->
+
+<!-- FOOTER (START) -->
+	<footer class="footer-distributed">
+		<div class="footer-left">
+
+			<h3>Company<span>logo</span></h3>
+
+			<p class="footer-links">
+				<a href="index.php">Home</a>
+				·
+				<a href="kamar.php">Kamar</a>
+				·
+				<a href="fasilitas.php">Fasilitas</a>
+				·
+				<a href="about.php">Tentang Kami</a>
+			</p>
+
+			<p class="footer-company-name">DEV &copy; 2019</p>
+		</div>
+
+		<div class="footer-center">
+
+			<div>
+				<p><span>Universitas Ahmad Dahlan</span>Yogyarkarta 2019</p>
+			</div>
+
+			<div>
+				<br>
+				Email us :
+				<br>
+				<p><a href="mailto:refaldi1700018013@webmail.uad.ac.id">refaldi1700018013@webmail.uad.ac.id</a></p>
+				<p><a href="mailto:aji1700018016@webmail.uad.ac.id">aji1700018016@webmail.uad.ac.id</a></p>
+				<p><a href="mailto:himawan1700018032@webmail.uad.ac.id">himawan1700018032@webmail.uad.ac.id</a></p>
+			</div>
+
+		</div>
+
+		<div class="footer-right">
+
+			<p class="footer-company-about">
+				<span>===Tentang Kami===</span>
+				Kelompok Kos-Kosan
+				<br>
+				Anggota :
+				<br>
+				1.) Refaldi Ergian (1700018013)
+				<br>
+				2.) Aji Gelar Prayogo (1700018016)
+				<br>
+				3.) Himawan Imtikhan Azmi (1700018032)
+			</p>
+		</div>
+	</footer>
+<!-- FOOTER (END) -->
