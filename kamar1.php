2e7ea34330ea73e23c89d489af5c2c15296e6e9e
RefaldiErgian <refaldi1700018013@webmail.uad.ac.id>
2019-05-03 13:14:46 +0700

    Update 03/05/2019

--- kamar1.php
+++ kamar1.php
@@ -1,8 +1,7 @@
-<?php 
-include "sql_connect.php";
-$key = 12315;
-$QueryString = "SELECT * FROM kamar WHERE no_ktp LIKE '%$key%' or id_kamar LIKE '%$key%' or no_kamar LIKE '%$key%' or luas_kamar LIKE '%$key%' or harga_kamar LIKE '%$key%' or status LIKE '%$key%'";
-	$SQL = mysqli_query($connect,$QueryString); 
+<?php /*
+include "sql_connect.php"; 
+$key = 1;
+	$SQL = $db->query("SELECT * FROM kamar WHERE no_kamar LIKE '%$key%'");
 ?>
 
 
@@ -36,34 +35,85 @@ $QueryString = "SELECT * FROM kamar WHERE no_ktp LIKE '%$key%' or id_kamar LIKE 
 		</div>
 	  </div>
 	</div>
-	
-<table border="1" align="center">
+
 		<tr>
-			<th>NO_KTP</th>
-			<th>ID_Kamar</th>
-			<th>NO_Kamar</th>
-			<th>Luas_Kamar</th>
-			<th>Harga_Kamar</th>
-			<th>Status</th>
-			
-		</tr>
 		<?php
 			foreach ($SQL as $data) {
 				echo "
-					<tr>
-						<td>$data[no_ktp]</td>
-						<td>$data[id_kamar]</td>
-						<td>$data[no_ktp]</td>
-						<td>$data[luas_kamar]</td>
-						<td>$data[harga_kamar]</td>
-						<td>$data[status]</td>
-					</tr>
+					<ul>
+						<li>$data[no_kamar]</li>
+						<li>$data[ukuran]</li>
+						<li>$data[harga]</li>
+						<li>$data[status]</li>
+						<li>$data[keterangan]</li>
+					</ul>
 				";
 			}
 		?>
-	</table>
-	
-<?php include('footer.php'); ?> 
+<?php include('footer.php');?> 
 
 </body>
 <html>
+*/ ?>
+
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" href="css/sewa.css">
+	<link href="css/index.css" type="text/css" rel="stylesheet" />
+	<link href="css/slideshow.css" type="text/css" rel="stylesheet" />
+	<script type="text/javascript" src="script/slideshow.js"></script>
+</head>
+<body>
+
+<?php include('header.php'); ?> 
+
+	<div class="w3-content" style="max-width:500px" style="max-height:200px">
+		<img class="mySlides" src="image/kamar/kamar1.jpeg" style="width:100%;display:none">
+		<img class="mySlides" src="image/kamar/kamar2.jpeg" style="width:100%">
+		<img class="mySlides" src="image/kamar/kamar3.jpeg" style="width:100%;display:none">
+
+		<div class="w3-row-padding w3-section">
+		
+		<div class="w3-col s4">
+		  <img class="demo w3-opacity w3-hover-opacity-off" src="image/kamar/kamar1.jpeg" style="width:100%;cursor:pointer" onclick="currentDiv(1)">
+		</div>
+		
+		<div class="w3-col s4">
+		  <img class="demo w3-opacity w3-hover-opacity-off" src="image/kamar/kamar2.jpeg" style="width:100%;cursor:pointer" onclick="currentDiv(2)">
+		</div>
+		
+		<div class="w3-col s4">
+		  <img class="demo w3-opacity w3-hover-opacity-off" src="image/kamar/kamar3.jpeg" style="width:100%;cursor:pointer" onclick="currentDiv(3)">
+		</div>
+	  </div>
+	</div>
+<?php
+require 'sql_connect.php';
+//List product dari database
+$sql = $db->query('SELECT * FROM kamar WHERE no_kamar = 1');
+ ?>
+<h2> Harga Sewa :</h2>
+ <table id="t01">
+ <tr>
+    <th>No Kamar</th>
+    <th>Ukuran</th>
+    <th>Harga</th>
+    <th>Status</th>
+    <th>Keterangan</th>
+ </tr>
+    <?php while($kamar = $sql->fetch(PDO::FETCH_OBJ)) { ?> 
+    <tr>
+        <td> <?php echo $kamar->no_kamar; ?> </td>
+        <td> <?php echo $kamar->ukuran; ?> </td>
+        <td> Rp.<?php echo $kamar->harga; ?> </td>
+        <td> <?php echo $kamar->status; ?> </td>
+        <td> <?php echo $kamar->keterangan; ?> </td>
+        <td> <a href="detail_sewa.php?id= <?php echo $kamar->no_kamar; ?> &action=add">Sewa Sekarang</a> </td>
+    </tr>
+    <?php } ?>
+ </table>
+
+<?php include('footer.php'); ?> 
+
+</body>
+</html>
